dde326063de9193d9f671407409714771c264dd5
balaforjava <pbcmmr1994@gmail.com>
2021-04-09 16:49:57 +0530

    ICalculator Implemented by CalculatorImpl class

---
+++ src/com/app/bala/impl/CalculatorImpl.java
@@ -0,0 +1,21 @@
+package com.app.bala.impl;
+
+import com.app.bala.ICalculator;
+
+public class CalculatorImpl implements ICalculator{
+
+	@Override
+	public int addTwoNumbers(int a, int b) {
+		int c=a+b;
+		return c;
+	}
+
+	@Override
+	public int substracktTwonumbers(int a, int b) {
+		int c=a-b;
+		return c;
+	}
+	
+	
+
+}
